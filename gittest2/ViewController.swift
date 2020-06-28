0ac5060f49bc27a4ab90b42662f9af38f3b63280
neallu <neallu@fourstonetech.com>
2020-06-28 16:43:39 +0800

    直播功能终于完成了

--- gittest2/ViewController.swift
+++ gittest2/ViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         print("做了个引导页")
+        print("做了直播功能")
+        print("终于把直播做好了")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(<#T##animated: Bool##Bool#>)
     }
 
 
